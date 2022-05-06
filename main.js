4151085e5c4fbe3de375cf1fcdfc118a7ceb8fb8
iediong <ediongi6@hotmail.com>
2022-05-06 14:16:57 +0100

    add javascript functionality for the toggle button

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-import './style.css'
+import './style.css';
 
+// Select Elements from the DOM
+const switchEl = document.querySelector('.switch');
+
+const toggleSwitch = function () {
+  const annualInput = document.getElementById('annually');
+  const slider = document.querySelector('.slider');
+  if (annualInput.checked) {
+    slider.style.setProperty('--left', '-26px');
+  } else {
+    slider.style.setProperty('--left', '0');
+  }
+};
+
+// Add an event listener to the slider
+switchEl.addEventListener('click', toggleSwitch);
