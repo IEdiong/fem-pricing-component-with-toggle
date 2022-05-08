e14e51311ed54fa0e96e9214e9cc14024aa39ec7
iediong <ediongi6@hotmail.com>
2022-05-08 11:30:03 +0100

    add functionality to update the price in the UI when the billing frequency is toggled

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ import './style.css';
 
 // Select Elements from the DOM
 const switchEl = document.querySelector('.switch');
+const basicPrice = document.getElementById('price-basic');
+const  proPrice = document.getElementById('price-professional');
+const masterPrice = document.getElementById('price-master');
 
 const toggleSwitch = function () {
   const annualInput = document.getElementById('annually');
   const slider = document.querySelector('.slider');
   if (annualInput.checked) {
     slider.style.setProperty('--left', '-26px');
+
+    // Update price in the UI
+    basicPrice.textContent = 199.99;
+    proPrice.textContent = 249.99;
+    masterPrice.textContent = 399.99;
   } else {
     slider.style.setProperty('--left', '0');
+
+    // Update price in the UI
+    basicPrice.textContent = 19.99;
+    proPrice.textContent = 24.99;
+    masterPrice.textContent = 39.99;
   }
 };
 
